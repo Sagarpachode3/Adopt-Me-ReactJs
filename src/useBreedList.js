98c2c2cfb82d62021094fbcb6eb6b971483b3ea3
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-03-08 18:47:31 +0530

    redux tool kit implementation. redux dev tool for chrome extension.

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,42 +1,14 @@
-//import { useState, useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchBreedList";
+import { useGetBreedsQuery } from "./petApiService";
 
 //const localCache = {};
 
 export default function useBreedList(animal) {
-  const results = useQuery(["breeds", animal], fetchBreedList);
-  return [results?.data?.breeds ?? [], results.status];
+  const { data: breeds, isLoading } = useGetBreedsQuery(animal, {
+    skip: !animal,
+  });
+  if (!animal) {
+    return [[], "loaded"];
+  }
+  // return [results?.data?.breeds ?? [], results.status];
+  return [breeds ?? [], isLoading ? "loading" : "loaded"];
 }
-
-
-
-// export default function useBreedList(animal) {
-//   const [breedList, setBreedList] = useState([]);
-//   const [status, setStatus] = useState("unloaded");
-
-//   useEffect(() => {
-//     if (!animal) {
-//       setBreedList([]);
-//     } else if (localCache[animal]) {
-//       setBreedList(localCache[animal]);
-//     } else {
-//       requestBreedList();
-//     }
-
-//     async function requestBreedList() {
-//       setBreedList([]);
-//       setStatus("loading");
-
-//       const res = await fetch(
-//         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-//       );
-//       const json = await res.json();
-//       localCache[animal] = json.breeds || [];
-//       setBreedList(localCache[animal]);
-//       setStatus("loaded");
-//     }
-//   }, [animal]);
-
-//   return [breedList, status];
-// }
